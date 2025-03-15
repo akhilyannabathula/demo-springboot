bb32df3c07232a959c337266ac8991bcd37e759b
akhilyannabathula <akhilkittu112233@gmail.com>
2025-03-15 15:08:20 +0530

    changed from String to List

--- src/main/java/com/example/demo/controllers/WarehouseController.java
+++ src/main/java/com/example/demo/controllers/WarehouseController.java
@@ -4,13 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class WarehouseController {
 
     @GetMapping("warehouses")
-    public String getAllWarehouses(){
-        return "123,356,789";
+    public List<Integer> getAllWarehouses(){
+        ArrayList<Integer> warehouses = new ArrayList<>();
+        warehouses.add(123);
+        warehouses.add(456);
+        warehouses.add(789);
+        return warehouses;
     }
 }
