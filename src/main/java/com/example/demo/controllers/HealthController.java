b54a9a0cee1a216fad1efa54b803e609be6f9e7c
akhilyannabathula <akhilkittu112233@gmail.com>
2025-03-15 14:30:09 +0530

    Added controllers

---
+++ src/main/java/com/example/demo/controllers/HealthController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health(){
+        return "{status : UP}";
+    }
+}
+
